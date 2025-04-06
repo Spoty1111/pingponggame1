b231e1e4f994fed2daf6ae1a4df4d6a75d3b9ae6
Spoty1111 <153323961+Spoty1111@users.noreply.github.com>
2025-04-06 13:23:47 +0500

    Add files via upload

--- maze.py
+++ maze.py
@@ -1,8 +1,24 @@
 from pygame import *
 from PyQt5 import Qt
 
+speed_x = 5
+speed_y = 5
+
+speed_x2 = 5
+speed_y = 5
 
 class GameSprite(sprite.Sprite):
+    def __init__(self,player_image,player_x,player_y,player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image),(30,200))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image,(self.rect.x,self.rect.y))
+
+class GameSprite2(sprite.Sprite):
     def __init__(self,player_image,player_x,player_y,player_speed):
         super().__init__()
         self.image = transform.scale(image.load(player_image),(65,65))
@@ -13,18 +29,23 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image,(self.rect.x,self.rect.y))
 
-class Player(GameSprite):
-    def update(self):
+class Player2(GameSprite):
+    def update2(self):
         keys_pressed = key.get_pressed()
         
         if keys_pressed[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys_pressed[K_DOWN] and self.rect.y < 395:
+        if keys_pressed[K_DOWN] and self.rect.y < 300:
             self.rect.y += self.speed
-        if keys_pressed[K_RIGHT] and self.rect.x < 595:
-            self.rect.x += self.speed
-        if keys_pressed[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
+
+class Player(GameSprite):
+    def update(self):
+        keys_pressed = key.get_pressed()
+        
+        if keys_pressed[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys_pressed[K_s] and self.rect.y < 300:
+            self.rect.y += self.speed
 
 
 class wall(sprite.Sprite):
@@ -50,12 +71,23 @@ background = transform.scale(image.load('newbackground.jpg'),(700,500))
 clock= time.Clock()
 FPS = 60
 
-ball = GameSprite('ball.png',350,250,5)
-wallie1 = pygame.Surface(0,255,0,30,250,430,200)
-wallie2 = wall(0,255,0,30,250,100,100)
+
+
+
+ball = GameSprite2('ball.png',350,250,5)
+
+ball2 = GameSprite('ball.png',250,250,5)
+
+wallie1 = Player('kitty.jpg',0,0,20)
+
+wallie2 = Player2('kitty.jpg',670,300,20)
+
 window = display.set_mode((700,500))
-display.set_caption('Лабиринт')
+
+display.set_caption('пингпонг')
+
 game = True
+
 finish = False
 
 while game:
@@ -64,10 +96,33 @@ while game:
             game = False
     if finish != True:
         window.blit(background,(0,0))
-        ball.reset()
-        window.blit(wallie1,(0,0))
+        ball.reset() 
+        
+        wallie1.update()
+        wallie1.reset()
+        wallie2.update2()
+        wallie2.reset()
 
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
 
+        ball2.rect.x += speed_x
+        ball2.rect.y += speed_y
+
+        if sprite.collide_rect(ball,wallie1):
+            speed_x *= -1.1
+
+        if sprite.collide_rect(ball,wallie2):
+            speed_x *= -1.1
+
+        if ball.rect.y > 425:
+            speed_y *= -1
+
+        if ball.rect.y < 0 :
+            speed_y *= -1
+        if ball.rect.y > 600:
+            window.blit(font1,(0,0))        
+        
 
     clock.tick(FPS)
     display.update()
